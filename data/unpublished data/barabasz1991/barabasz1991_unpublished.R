71c0a152d24711ba8dfab9f460a1e786c7074aea
Nicholas A. Coles <40498285+ColesNicholas@users.noreply.github.com>
2022-09-20 14:28:10 -0700

    Moved over unpublished data

---
+++ data/unpublished data/barabasz1991/barabasz1991_unpublished.R
@@ -0,0 +1,17 @@
+# Manual calculation of M's and SD's from data reported in Table 2 
+# of 'Effects of experimental context, demand characteristics, and 
+# situational cues: New data'
+
+# experimental subject data
+exp <- c(2, 0, 0, 0, 1, 
+         0, 0, 0, 0, 0)
+
+mean(exp)
+sd(exp)
+
+# control group data
+cont <- c(4, 0, 0, 0, 0, 
+          0, 3, 0, 0, 0)
+
+mean(cont)
+sd(cont)
